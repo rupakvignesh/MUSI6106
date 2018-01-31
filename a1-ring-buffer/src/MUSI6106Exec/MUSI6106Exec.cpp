bb9b51fa0a1b8e3495115e0032a513da6e11c87a
unknown <jyotinarang.nsit@gmail.com>
2018-01-31 09:01:58 -0500

    Error check modified

--- a1-ring-buffer/src/MUSI6106Exec/MUSI6106Exec.cpp
+++ a1-ring-buffer/src/MUSI6106Exec/MUSI6106Exec.cpp
@@ -11,97 +11,97 @@ using std::cout;
 using std::endl;
 
 // local function declarations
-void    showClInfo ();
+void    showClInfo();
 
-void testFillBuffer(CRingBuffer<float> *pCRingBuffer, float* pfTestSignal, int kBlockSize){
-    for(int i=0;i<kBlockSize;i++){
-        pCRingBuffer->putPostInc(pfTestSignal[i]);
-    }
-    int sum = 0;
-    for(int i=0;i<3*kBlockSize;i++){
-        if (pCRingBuffer->getPostInc() == pfTestSignal[i%kBlockSize]) sum += 1;
-    }
-    if(sum==3*kBlockSize){
-        cout<<"Fill buffer test case Passed"<<endl;
-    }
-    else{
-        cout<<"Test case failed"<<endl;
-    }
+void testFillBuffer(CRingBuffer<float> *pCRingBuffer, float* pfTestSignal, int kBlockSize) {
+	for (int i = 0;i<kBlockSize;i++) {
+		pCRingBuffer->putPostInc(pfTestSignal[i]);
+	}
+	int sum = 0;
+	for (int i = 0;i<3 * kBlockSize;i++) {
+		if (pCRingBuffer->getPostInc() == pfTestSignal[i%kBlockSize]) sum += 1;
+	}
+	if (sum == 3 * kBlockSize) {
+		cout << "Fill buffer test case Passed" << endl;
+	}
+	else {
+		cout << "Test case failed" << endl;
+	}
 }
 
-void testMethods(CRingBuffer<float> *pCRingBuffer, float* pfTestSignal, int kBlockSize){
-    int prod = 1;
-    pCRingBuffer->reset();                                      //w=0, r=0
-    if(pCRingBuffer->getWriteIdx() != 0) prod = 0;              //w=0
-    pCRingBuffer->put(521);
-    if(pCRingBuffer->getReadIdx() != 0) prod = 0 ;               //r=0
-    pCRingBuffer->setReadIdx(pCRingBuffer->getWriteIdx());      //r=0
-    if(pCRingBuffer->get(0) != 521) prod = 0;                           //521
-    pCRingBuffer->setWriteIdx(kBlockSize-1);
-    pCRingBuffer->setReadIdx(kBlockSize-1);
-    if(pCRingBuffer->getReadIdx() != kBlockSize-1 && pCRingBuffer->getWriteIdx() != kBlockSize-1) prod=0;      //r=kBlockSize-1
-    if(prod==1){
-        cout<<"Test cases for methods passed"<<endl;
-    }
-    else{
-        cout<<"Test case failed"<<endl;
-    }
+void testMethods(CRingBuffer<float> *pCRingBuffer, float* pfTestSignal, int kBlockSize) {
+	int prod = 1;
+	pCRingBuffer->reset();                                      //w=0, r=0
+	if (pCRingBuffer->getWriteIdx() != 0) prod = 0;              //w=0
+	pCRingBuffer->put(521);
+	if (pCRingBuffer->getReadIdx() != 0) prod = 0;               //r=0
+	pCRingBuffer->setReadIdx(pCRingBuffer->getWriteIdx());      //r=0
+	if (pCRingBuffer->get(0) != 521) prod = 0;                           //521
+	pCRingBuffer->setWriteIdx(kBlockSize - 1);
+	pCRingBuffer->setReadIdx(kBlockSize - 1);
+	if (pCRingBuffer->getReadIdx() != kBlockSize - 1 && pCRingBuffer->getWriteIdx() != kBlockSize - 1) prod = 0;      //r=kBlockSize-1
+	if (prod == 1) {
+		cout << "Test cases for methods passed" << endl;
+	}
+	else {
+		cout << "Test case failed" << endl;
+	}
 
 }
 
-void testEmptyFull(CRingBuffer<float> *pCRingBuffer, float* pfTestSignal, int kBlockSize){
-    pCRingBuffer->reset();
-    int prod = 1;
-    if(pCRingBuffer->getNumValuesInBuffer() != 0){            //Empty Buffer
-        prod = 0;
-    }
-    for(int i=0;i<kBlockSize;i++){                          //Fill in values
-        pCRingBuffer->putPostInc(pfTestSignal[i]);
-    }
-    if(pCRingBuffer->getNumValuesInBuffer() != kBlockSize){   //Buffer full
-        prod = 0;
-    }
-    
-    if(prod==1){
-        cout<<"Test case Empty/Full buffer Passed"<<endl;
-    }
-    else{
-        cout<<"Test case Empty/Full buffer failed"<<endl;
-    }
-    
+void testEmptyFull(CRingBuffer<float> *pCRingBuffer, float* pfTestSignal, int kBlockSize) {
+	pCRingBuffer->reset();
+	int prod = 1;
+	if (pCRingBuffer->getNumValuesInBuffer() != 0) {            //Empty Buffer
+		prod = 0;
+	}
+	for (int i = 0;i<kBlockSize;i++) {                          //Fill in values
+		pCRingBuffer->putPostInc(pfTestSignal[i]);
+	}
+	if (pCRingBuffer->getNumValuesInBuffer() != kBlockSize) {   //Buffer full
+		prod = 0;
+	}
+
+	if (prod == 1) {
+		cout << "Test case Empty/Full buffer Passed" << endl;
+	}
+	else {
+		cout << "Test case Empty/Full buffer failed" << endl;
+	}
+
 }
 
-void testOffsetNegative(CRingBuffer<float> *pCRingBuffer,float* pfTestSignal, int kBlockSize){
-    pCRingBuffer->reset();
-    for(int i=0; i<kBlockSize; i++){                          //Fill in values
-        pCRingBuffer->putPostInc(pfTestSignal[i]);
-    }
-    int flag = 0;
-    for(int i=0; i<kBlockSize; i++){
-        if(pCRingBuffer->get(-i-1) != pfTestSignal[kBlockSize-i-1]){
-            flag = 1;
-            break;
-        }
-    }
-    
-    if(flag==0){
-        cout<<"Test case offset negative passed"<<endl;
-    }
-    else{
-        cout<<"Test case offset negative failed"<<endl;
-    }
-    
+void testOffsetNegative(CRingBuffer<float> *pCRingBuffer, float* pfTestSignal, int kBlockSize) {
+	pCRingBuffer->reset();
+	for (int i = 0; i<kBlockSize; i++) {                          //Fill in values
+		pCRingBuffer->putPostInc(pfTestSignal[i]);
+	}
+	int flag = 0;
+	for (int i = 0; i<kBlockSize; i++) {
+		if (pCRingBuffer->get(-i - 1) != pfTestSignal[kBlockSize - i - 1]) {
+			flag = 1;
+			break;
+		}
+	}
+
+	if (flag == 0) {
+		cout << "Test case offset negative passed" << endl;
+	}
+	else {
+		cout << "Test case offset negative failed" << endl;
+	}
+
 }
 
-void testIndexCrossing(CRingBuffer<float> *pCRingBuffer,float* pfTestSignal, int kBlockSize){
-    pCRingBuffer->reset();
-    for(int i=0; i<kBlockSize; i++){                          //Fill in values
-        pCRingBuffer->putPostInc(pfTestSignal[i]);
-    }
-    
-    
-    pCRingBuffer->putPostInc(0);
-    
+void testIndexCrossing(CRingBuffer<float> *pCRingBuffer, float* pfTestSignal, int kBlockSize) {
+	pCRingBuffer->reset();
+	for (int i = 0; i<kBlockSize; i++) {                          //Fill in values
+		pCRingBuffer->putPostInc(pfTestSignal[i]);
+	}
+
+
+	pCRingBuffer->putPostInc(0);
+
 }
 
 
@@ -110,73 +110,73 @@ void testIndexCrossing(CRingBuffer<float> *pCRingBuffer,float* pfTestSignal, int
 // main function
 int main(int argc, char* argv[])
 {
-    // You don't need to use an audio file this time, though you can try if you want.
-    // std::string             sInputFilePath,                 //!< file paths
-    //                         sOutputFilePath;
-     static const int        kBlockSize          = 8;
-    // float                   **ppfAudioData      = 0;
-    // CAudioFileIf            *phAudioFile        = 0;
-    // CAudioFileIf::FileSpec_t stFileSpec;
-    // long long               iInFileLength       = 0;        //!< length of input file
+	// You don't need to use an audio file this time, though you can try if you want.
+	// std::string             sInputFilePath,                 //!< file paths
+	//                         sOutputFilePath;
+	static const int        kBlockSize = 8;
+	// float                   **ppfAudioData      = 0;
+	// CAudioFileIf            *phAudioFile        = 0;
+	// CAudioFileIf::FileSpec_t stFileSpec;
+	// long long               iInFileLength       = 0;        //!< length of input file
 
-    clock_t                 time                = 0;
+	clock_t                 time = 0;
 
-    CRingBuffer<float>      *pCRingBuffer       = 0;
-    float                   *pfTestSignal       = 0;
-    //float                   fGain               = 1.f;
+	CRingBuffer<float>      *pCRingBuffer = 0;
+	float                   *pfTestSignal = 0;
+	//float                   fGain               = 1.f;
 
-    showClInfo ();
+	showClInfo();
 
-    //////////////////////////////////////////////////////////////////////////////
-    // allocate memory
-    time                    = clock();
-    pCRingBuffer = new CRingBuffer<float> (kBlockSize);
+	//////////////////////////////////////////////////////////////////////////////
+	// allocate memory
+	time = clock();
+	pCRingBuffer = new CRingBuffer<float>(kBlockSize);
 
-    // fill the test signal (e.g., a unit impulse)
-    pfTestSignal = new float [kBlockSize];
-    pfTestSignal[0] = 1;                            // Unit impulse
-    for(int i=1;i<kBlockSize;i++){
-        pfTestSignal[i] = 0;
-    }
-    //////////////////////////////////////////////////////////////////////////////
-    // do processing and tests
-    
-    //Test case I. Fill the buffer with test signal and output values.
-    cout<<"Test case I. Fill the buffer with test signal and check circular functionality "<<endl;
-    testFillBuffer(pCRingBuffer, pfTestSignal, kBlockSize);
-    
-    //Test case II. Function calls.
-    cout<<"Test case II. Function calls."<<endl;
-    testMethods(pCRingBuffer, pfTestSignal, kBlockSize);
-    
-    //Test case III. Empty and full buffers
-    cout<<"Test case III. Empty and Full buffers."<<endl;
-    testEmptyFull(pCRingBuffer, pfTestSignal, kBlockSize);
-    
-    //Test case IV. Negative offsets.
-    cout<<"Test case IV. Negative offsets"<<endl;
-    testOffsetNegative(pCRingBuffer, pfTestSignal, kBlockSize);
-    
-    //Test case V. Read write index crossing.
-    cout<<"Test case V. Read write index crossing"<<endl;
-    testIndexCrossing(pCRingBuffer, pfTestSignal, kBlockSize);
+	// fill the test signal (e.g., a unit impulse)
+	pfTestSignal = new float[kBlockSize];
+	pfTestSignal[0] = 1;                            // Unit impulse
+	for (int i = 1;i<kBlockSize;i++) {
+		pfTestSignal[i] = 0;
+	}
+	//////////////////////////////////////////////////////////////////////////////
+	// do processing and tests
 
-    cout << "processing done in: \t"    << (clock()-time)*1.F/CLOCKS_PER_SEC << " seconds." << endl;
+	//Test case I. Fill the buffer with test signal and output values.
+	cout << "Test case I. Fill the buffer with test signal and check circular functionality " << endl;
+	testFillBuffer(pCRingBuffer, pfTestSignal, kBlockSize);
 
-    //////////////////////////////////////////////////////////////////////////////
-    // clean-up
-    delete pCRingBuffer;
-    
-    return 0;
+	//Test case II. Function calls.
+	cout << "Test case II. Function calls." << endl;
+	testMethods(pCRingBuffer, pfTestSignal, kBlockSize);
+
+	//Test case III. Empty and full buffers
+	cout << "Test case III. Empty and Full buffers." << endl;
+	testEmptyFull(pCRingBuffer, pfTestSignal, kBlockSize);
+
+	//Test case IV. Negative offsets.
+	cout << "Test case IV. Negative offsets" << endl;
+	testOffsetNegative(pCRingBuffer, pfTestSignal, kBlockSize);
+
+	//Test case V. Read write index crossing.
+	cout << "Test case V. Read write index crossing" << endl;
+	testIndexCrossing(pCRingBuffer, pfTestSignal, kBlockSize);
+
+	cout << "processing done in: \t" << (clock() - time)*1.F / CLOCKS_PER_SEC << " seconds." << endl;
+
+	//////////////////////////////////////////////////////////////////////////////
+	// clean-up
+	delete pCRingBuffer;
+
+	return 0;
 }
 
 
 void     showClInfo()
 {
-    cout << "GTCMT MUSI6106 Executable" << endl;
-    cout << "(c) 2014-2018 by Alexander Lerch" << endl;
-    cout  << endl;
+	cout << "GTCMT MUSI6106 Executable" << endl;
+	cout << "(c) 2014-2018 by Alexander Lerch" << endl;
+	cout << endl;
 
-    return;
+	return;
 }
 
--- a1-ring-buffer/src/inc/RingBuffer.h
+++ a1-ring-buffer/src/inc/RingBuffer.h
@@ -7,160 +7,175 @@
 
 /*! \brief implement a circular buffer of type T
 */
-template <class T> 
+template <class T>
 class CRingBuffer
 {
 public:
-    explicit CRingBuffer (int iBufferLengthInSamples) :
-        m_iBuffLength(iBufferLengthInSamples),
-        m_iReadIdx(0),                          
-        m_iWriteIdx(0),
-        m_ptBuff(0)
-    {
-        assert(iBufferLengthInSamples > 0);
+	explicit CRingBuffer(int iBufferLengthInSamples) :
+		m_iBuffLength(iBufferLengthInSamples),
+		m_iReadIdx(0),
+		m_iWriteIdx(0),
+		m_ptBuff(0)
+	{
+		assert(iBufferLengthInSamples > 0);
 
-        m_ptBuff        = new T [m_iBuffLength];
-        reset();
-    }
+		m_ptBuff = new T[m_iBuffLength];
+		reset();
+	}
 
-    virtual ~CRingBuffer ()
-    {
-        delete [] m_ptBuff;
-        m_ptBuff    = 0;
-    }
+	virtual ~CRingBuffer()
+	{
+		delete[] m_ptBuff;
+		m_ptBuff = 0;
+	}
 
-    /*! add a new value of type T to write index
-    \param tNewValue the new value
-    \return void
-    */
-    void put(T tNewValue)
-    {
-        m_ptBuff[m_iWriteIdx] = tNewValue;
-    }
-    
-    /*! return the value at the current read index
-    \return type T the value from the read index
-    */
-//    T get () const
-//    {
-//        return m_ptBuff[m_iReadIdx];
-//    }
+	/*! add a new value of type T to write index
+	\param tNewValue the new value
+	\return void
+	*/
+	void put(T tNewValue)
+	{
+		m_ptBuff[m_iWriteIdx] = tNewValue;
+	}
 
-    /*! return the current index for writing/put
-    \return int
-    */
-    int getWriteIdx () const
-    {
-        return m_iWriteIdx;
-    }
+	/*! return the value at the current read index
+	\return type T the value from the read index
+	*/
+	//    T get () const
+	//    {
+	//        return m_ptBuff[m_iReadIdx];
+	//    }
 
-    /*! move the write index to a new position
-    \param iNewWriteIdx: new position
-    \return void
-    */
-    void setWriteIdx (int iNewWriteIdx)
-    {
-        m_iWriteIdx = iNewWriteIdx;
-        assertReadWrite();
-        
-    }
+	/*! return the current index for writing/put
+	\return int
+	*/
+	int getWriteIdx() const
+	{
+		return m_iWriteIdx;
+	}
 
-    /*! return the current index for reading/get
-    \return int
-    */
-    int getReadIdx () const
-    {
-        return m_iReadIdx;
-    }
+	/*! move the write index to a new position
+	\param iNewWriteIdx: new position
+	\return void
+	*/
+	void setWriteIdx(int iNewWriteIdx)
+	{
+		m_iWriteIdx = iNewWriteIdx;
+		int flag = checkReadWrite();
+		if (flag == 1) {
+			cout << "Buffer full. Will overwrite now.";
 
-    /*! move the read index to a new position
-    \param iNewReadIdx: new position
-    \return void
-    */
-    void setReadIdx (int iNewReadIdx)
-    {
-        m_iReadIdx = iNewReadIdx;
-        assertReadWrite();
-        
-    }
+		}
 
-    /*! add a new value of type T to write index and increment write index
-    \param tNewValue the new value
-    \return void
-    */
-    void putPostInc (T tNewValue)
-    {
-        m_ptBuff[mod(m_iWriteIdx++, m_iBuffLength)] = tNewValue;
-        assertReadWrite();
-        
-    }
+	}
 
-    /*! return the value at the current read index and increment the read pointer
-    \return type T the value from the read index
-    */
-    T getPostInc ()
-    {
-        return m_ptBuff[mod(m_iReadIdx++, m_iBuffLength)];
-        assertReadWrite();
-        
-    }
+	/*! return the current index for reading/get
+	\return int
+	*/
+	int getReadIdx() const
+	{
+		return m_iReadIdx;
+	}
 
-    /*! return the value at the index with an arbitrary offset
-    \param iOffset: read at offset from read index
-    \return type T the value from the read index
-    */
-    T get (int iOffset = 0) const
-    {
-        return m_ptBuff[mod(m_iReadIdx+iOffset, m_iBuffLength)];
-    }
-    
-    /*! set buffer content and indices to 0
-    \return void
-    */
-    void reset ()
-    {
-        memset (m_ptBuff, 0, sizeof(T)*m_iBuffLength);
-        m_iReadIdx  = 0;
-        m_iWriteIdx = 0;
-    }
+	/*! move the read index to a new position
+	\param iNewReadIdx: new position
+	\return void
+	*/
+	void setReadIdx(int iNewReadIdx)
+	{
+		m_iReadIdx = iNewReadIdx;
+		int flag = checkReadWrite();
+		if (flag == 1) {
+			cout << "Buffer full. Will overwrite now.";
+		}
 
-    /*! returns the number of values currently buffered (note: 0 could also mean the buffer is full!)
-    \return int
-    */
-    int getNumValuesInBuffer () const
-    {
-        return m_iWriteIdx-m_iReadIdx;      //(m_iWriteIdx - m_iReadIdx + m_iBuffLength)%m_iBuffLength;
-    }
+	}
 
-    /*! returns the length of the internal buffer
-    \return int
-    */
-    int getLength () const 
-    {
-        return m_iBuffLength;
-    }
+	/*! add a new value of type T to write index and increment write index
+	\param tNewValue the new value
+	\return void
+	*/
+	void putPostInc(T tNewValue)
+	{
+		m_ptBuff[mod(m_iWriteIdx++, m_iBuffLength)] = tNewValue;
+		int flag = checkReadWrite();
+		if (flag == 1) {
+			cout << "Buffer full. Will overwrite now.";
+			char c = getchar();
+		}
+
+	}
+
+	/*! return the value at the current read index and increment the read pointer
+	\return type T the value from the read index
+	*/
+	T getPostInc()
+	{
+		return m_ptBuff[mod(m_iReadIdx++, m_iBuffLength)];
+		int flag = checkReadWrite();
+		if (flag == 1) {
+			cout << "Buffer full. Will overwrite now.";
+		}
+
+	}
+
+	/*! return the value at the index with an arbitrary offset
+	\param iOffset: read at offset from read index
+	\return type T the value from the read index
+	*/
+	T get(int iOffset = 0) const
+	{
+		return m_ptBuff[mod(m_iReadIdx + iOffset, m_iBuffLength)];
+	}
+
+	/*! set buffer content and indices to 0
+	\return void
+	*/
+	void reset()
+	{
+		memset(m_ptBuff, 0, sizeof(T)*m_iBuffLength);
+		m_iReadIdx = 0;
+		m_iWriteIdx = 0;
+	}
+
+	/*! returns the number of values currently buffered (note: 0 could also mean the buffer is full!)
+	\return int
+	*/
+	int getNumValuesInBuffer() const
+	{
+		return m_iWriteIdx - m_iReadIdx;      //(m_iWriteIdx - m_iReadIdx + m_iBuffLength)%m_iBuffLength;
+	}
+
+	/*! returns the length of the internal buffer
+	\return int
+	*/
+	int getLength() const
+	{
+		return m_iBuffLength;
+	}
 private:
-    CRingBuffer ();
-    CRingBuffer(const CRingBuffer& that);
+	CRingBuffer();
+	CRingBuffer(const CRingBuffer& that);
 
-    int m_iBuffLength,              //!< length of the internal buffer
-        m_iReadIdx,                 //!< current read index
-        m_iWriteIdx;                //!< current write index
+	int m_iBuffLength,              //!< length of the internal buffer
+		m_iReadIdx,                 //!< current read index
+		m_iWriteIdx;                //!< current write index
 
-    T   *m_ptBuff;                  //!< data buffer
-    
-    int mod(int a, int b) const{
-        int c = a%b;
-        return (c<0) ? c+b : c;
-    }
-    //r,w should not be negative.
-    //w should not overcross read.
-    void assertReadWrite(){
-        assert(m_iReadIdx >= 0);
-        assert(m_iWriteIdx >= 0);
-        assert(m_iWriteIdx - m_iReadIdx <= m_iBuffLength);
-        assert(m_iWriteIdx - m_iReadIdx >=0);
-    }
-    
+	T   *m_ptBuff;                  //!< data buffer
+
+	int mod(int a, int b) const {
+		int c = a%b;
+		return (c<0) ? c + b : c;
+	}
+	//r,w should not be negative.
+	//w should not overcross read.
+	int checkReadWrite() {
+		int flag = 0;
+		if ((m_iReadIdx < 0) || (m_iWriteIdx < 0) || (m_iWriteIdx - m_iReadIdx > m_iBuffLength) || (m_iWriteIdx - m_iReadIdx < 0)) {
+			flag = 1;
+		}
+		return flag;
+	}
+
 };
 #endif // __RingBuffer_hdr__
